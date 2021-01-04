c16fe4ee07ae62ea199a2c86e936163da417b379
justynag7 <48431729+justynag7@users.noreply.github.com>
2021-01-04 17:23:19 +0100

    Add files via upload

---
+++ text_transformer/Decorator.java
@@ -0,0 +1,25 @@
+package textTransformers.models;
+
+
+/*
+ * Pattern class 
+ * */
+public class Decorator implements ITextParser{
+	
+	protected ITextParser input;
+
+	public Decorator(ITextParser input) {
+		super();
+		this.input = input;
+	}
+
+	
+	//Zwraca tekst po transformacji
+	@Override
+	public String transform() {
+		return this.input.transform();
+	}
+	
+	
+	
+}
